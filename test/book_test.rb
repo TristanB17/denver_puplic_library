a5df6a5b5b8246227da603eef8f4c1c23ee9795c
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-11 09:12:44 -0600

    First iteration completed, all tests function properly.

--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,28 @@
 require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/book'
 
 class BookTest < Minitest::Test
 
+  def test_it_exists
+    book = Book.new({
+      author_first_name: "Harper",
+      author_last_name: "Lee",
+      title: "To Kill a Mockingbird",
+      publication_date: "July 11, 1960"
+      })
+    assert_instance_of Book, book
+  end
 
+  def test_a_book_has_attributes
+    book = Book.new({
+      author_first_name: "Harper",
+      author_last_name: "Lee",
+      title: "To Kill a Mockingbird",
+      publication_date: "July 11, 1960"
+      })
+    assert_equal "Harper", book.author_first_name
+    assert_equal "To Kill a Mockingbird", book.title
+  end
 end
