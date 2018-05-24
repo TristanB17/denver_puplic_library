cffa79dee65e97ddb663ee1e769444bafde46e9b
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-05-24 11:38:54 -0600

    All iterations complette.

--- lib/library.rb
+++ lib/library.rb
@@ -7,5 +7,40 @@ class Library
 
   def add_to_collection(book)
     @books << book
-  end 
+  end
+
+  def include?(work)
+    titles = @books.map do |book|
+      book.title
+    end
+    titles.include?(work)
+  end
+
+  def card_catalogue
+    alpha = @books.map do |book|
+      book.author_last_name
+    end
+    alpha.sort
+  end
+
+  def find_by_author(author)
+    author_last = author.split(" ")[1]
+    author_hash = {}
+    @books.map do |book|
+      if book.author_last_name == author_last
+        author_hash[book.title] = book
+      end
+    end
+    author_hash
+  end
+
+  def find_by_publication_date(date)
+    date_hash = {}
+    years =  @books.map do |book|
+      if book.publication_date[-4..-1] == date
+        date_hash[book.publication_date[-4..-1]] = book
+      end
+    end
+    date_hash
+  end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -38,4 +38,118 @@ class LibraryTest < Minitest::Test
     assert_equal mockingbird, dpl.books[1]
     assert_equal villette, dpl.books[2]
   end
+
+  def test_library_can_find_book_by_book_info
+    charlotte_bronte = Author.new({
+      first_name: "Charlotte",
+      last_name: "Bronte"
+      })
+
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+
+    harper_lee  = Author.new({
+      first_name: "Harper",
+      last_name: "Lee"
+      })
+
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl = Library.new
+
+    dpl.add_to_collection(jane_eyre)
+    dpl.add_to_collection(mockingbird)
+    dpl.add_to_collection(villette)
+
+    assert_equal true, dpl.include?("To Kill a Mockingbird")
+    assert_equal false, dpl.include?("A Connecticut Yankee in King Arthur's Court")
+  end
+
+  def test_library_can_alphabetize_books_by_last_name
+    charlotte_bronte = Author.new({
+      first_name: "Charlotte",
+      last_name: "Bronte"
+      })
+
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+
+    harper_lee  = Author.new({
+      first_name: "Harper",
+      last_name: "Lee"
+      })
+
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl = Library.new
+
+    dpl.add_to_collection(jane_eyre)
+    dpl.add_to_collection(mockingbird)
+    dpl.add_to_collection(villette)
+
+    alphabetical = dpl.card_catalogue
+
+    assert_equal "Bronte", alphabetical[0]
+  end
+
+  def test_library_can_find_book_by_book_info
+    charlotte_bronte = Author.new({
+      first_name: "Charlotte",
+      last_name: "Bronte"
+      })
+
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+
+    harper_lee  = Author.new({
+      first_name: "Harper",
+      last_name: "Lee"
+      })
+
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl = Library.new
+
+    dpl.add_to_collection(jane_eyre)
+    dpl.add_to_collection(mockingbird)
+    dpl.add_to_collection(villette)
+
+    alphabetical = dpl.card_catalogue
+
+    assert_equal "Bronte", alphabetical[0]
+  end
+
+  def test_library_can_search_for_book_with_book_info
+    charlotte_bronte = Author.new({
+      first_name: "Charlotte",
+      last_name: "Bronte"
+      })
+
+    jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    villette  = charlotte_bronte.add_book("Villette", "1853")
+
+    harper_lee  = Author.new({
+      first_name: "Harper",
+      last_name: "Lee"
+      })
+
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl = Library.new
+
+    dpl.add_to_collection(jane_eyre)
+    dpl.add_to_collection(mockingbird)
+    dpl.add_to_collection(villette)
+
+    return_author = dpl.find_by_author("Charlotte Bronte")
+
+    assert_equal Hash, return_author.class
+    assert_equal "Jane Eyre", return_author.keys[0]
+    assert_equal "Villette", return_author.keys[1]
+
+    return_date = dpl.find_by_publication_date("1960")
+
+    assert_equal Hash, return_date.class
+    assert_equal "1960", return_date.keys[0]
+  end
 end
